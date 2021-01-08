0b30d5dd1a8a55bb9b90ba83c19ae172845e0e43
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2021-01-08 19:08:44 +0700

    abc

--- DA_Android/app/src/main/java/com/example/da_android/CaptureAct.java
+++
@@ -1,6 +0,0 @@
-package com.example.da_android;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class CaptureAct extends CaptureActivity {
-}
--- DA_Android/app/src/main/java/com/example/da_android/Constant.java
+++
@@ -1,5 +0,0 @@
-package com.example.da_android;
-
-public class Constant {
-    public static String PATH = "http://499c0b5b683a.ngrok.io/";
-}
